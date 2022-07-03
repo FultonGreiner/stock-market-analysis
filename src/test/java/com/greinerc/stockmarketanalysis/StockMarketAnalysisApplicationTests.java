74a818742c597fa5d1b6859f06e125e715e1702f
greinerc <greinerc@purdue.edu>
2022-07-02 19:42:06 -0700

    Initial commit

---
+++ src/test/java/com/greinerc/stockmarketanalysis/StockMarketAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greinerc.stockmarketanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
