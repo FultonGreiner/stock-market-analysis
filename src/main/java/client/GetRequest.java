13b0a53687ebc2830f93eb0f2508cacb85d2389b
greinerc <greinerc@purdue.edu>
2022-07-02 22:05:15 -0700

    GetRequest

--- src/main/java/client/GetRequest.java
+++ src/main/java/client/GetRequest.java
@@ -1,4 +1,18 @@
 package client;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.net.URL;
+
+@AllArgsConstructor
 public class GetRequest {
+    @Getter @Setter private String apiKey;
+    @Getter @Setter private URL url;
+
+    public static void getRequest() {
+
+    }
+
 }
